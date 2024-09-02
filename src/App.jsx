837b110aabc631297b706f22274b85e83b67b6e8
Salman-Nazeer <nazeersalman716@gmil.com>
2024-09-02 15:13:07 -0500

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import Home from "./pages/Home";
-import Favorites from "./pages/Favorites"
+import Favorites from "./pages/Favorites";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <div className="flex">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Favorites" element={<Favorites />} />
-        </Routes>
-      </div>
+      <BrowserRouter>
+        <div className="flex">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Favorites" element={<Favorites />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
